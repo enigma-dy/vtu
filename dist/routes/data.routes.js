c851f9c8daaaa1659a7b1351811b40bc16700b71
enigma-dy <oluwagbengaadedire@gmail.com>
2025-06-29 10:43:45 +0100

    update

--- dist/routes/data.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const vtu_controller_1 = require("../controllers/vtu.controller");
-const router = (0, express_1.Router)();
-router.post('/data', vtu_controller_1.setDataPrices);
-router.get('/fetch', vtu_controller_1.fetchVtuDataPricing);
-router.get('/data', vtu_controller_1.getVtuDataPrices);
-exports.default = router;
